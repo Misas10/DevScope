ca651eb6e70def4caf386b5702c6253b621438b9
Misael Armando <misael.armando@gls-portugal.com>
2026-04-22 01:46:24 +0100

    Feature: code structure and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'repos',
+    loadComponent: () => import('./pages/repository/list').then(c => c.RepositoryComponent),
+  },
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/repos',
+  }
+];
---
+++ src/app/pages/repository/list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repository',
+  imports: [],
+  templateUrl: './list.html',
+  styleUrl: './list.scss',
+})
+export class RepositoryComponent {
+
+}
